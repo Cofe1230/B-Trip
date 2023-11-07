f7f429faaa172c8b776f6d30a06ac9da0d8ab0f2
Cofe1230 <winner1230@gmail.com>
2023-11-07 10:33:06 +0900

    백업

---
+++ SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/BTripApplication.java
@@ -0,0 +1,13 @@
+package com.dataproject.btrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTripApplication.class, args);
+	}
+
+}
