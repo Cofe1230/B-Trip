64bfe553a480b4e6d5d14eaed9fc527279b0ca70
Cofe1230 <winner1230@gmail.com>
2023-11-09 15:52:42 +0900

    update

--- SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/service/OverviewService.java
+++ SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/service/OverviewService.java
@@ -2,7 +2,6 @@ package com.dataproject.btrip.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dataproject.btrip.entity.ForiegnVisitors;
@@ -13,18 +12,17 @@ import com.dataproject.btrip.repository.SpotVisitorsRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class OverviewService {
-	@Autowired
-    private ForiegnVisitorsRepository foriegnVisitorsRepository;
-	@Autowired
-    private SpotVisitorsRepository spotVisitorsRepository;
-
-    public List<ForiegnVisitors> getForiegnVisitors() {
-        return foriegnVisitorsRepository.findAll();
-    }
-
-    public List<SpotVisitors> getSpotVisitors() {
-        return spotVisitorsRepository.findAll();
-    }
+	private final ForiegnVisitorsRepository foriegnVisitorsRepository;
+	private final SpotVisitorsRepository spotVisitorsRepository;
+	
+	public List<ForiegnVisitors> getForiegnVisitors(){
+		return foriegnVisitorsRepository.findAll();
+	}
+	
+	public List<SpotVisitors> getSpotVisitors(){
+		return spotVisitorsRepository.findAll();
+	}
+	
 }
-
