24c540e08e1ca7c3e0afec0f6af0b1c889c6e9cc
Cofe1230 <winner1230@gmail.com>
2023-11-09 20:01:57 +0900

    pom update

--- SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/configuration/WebConfig.java
+++ SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("https://13.125.213.174","http://localhost:3000/")
+		.allowedOrigins("http://localhost:80","http://localhost:3000")
 		.allowedMethods("GET", "POST");
 	}
 }
