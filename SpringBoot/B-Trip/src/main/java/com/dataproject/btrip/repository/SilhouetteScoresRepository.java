de865fc6ab002ca6e404b9e494a28fa982d44ccd
Cofe1230 <winner1230@gmail.com>
2023-11-10 14:46:21 +0900

    back spring

--- SpringBoot/B-Trip/src/main/java/com/dataproject/btrip/repository/SilhouetteScoresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dataproject.btrip.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dataproject.btrip.entity.SilhouetteScores;
-
-public interface SilhouetteScoresRepository extends JpaRepository<SilhouetteScores, Long> {
-
-}
